cd78780c08f1c69698cd93041b89df9d83e0c5a0
vaibhav kumar <54022903+vaibhavkumar1@users.noreply.github.com>
2021-11-16 12:10:30 +0530

    Add files via upload

--- route.js
+++ route.js
@@ -7,17 +7,17 @@ const student=require('../models/student');
 
 router.post("/student",async (req,res)=>{
     try{
-        const getPreviouse_Id=await student.find().sort({_id:-1});
-        const firstId=_.get(_.first(getPreviouse_Id),'e_Id',999);
-        const newId=firstId+1;
-        Object.assign(req.body,{e_Id:newId});
+        // const getPreviouse_Id=await student.find().sort({_id:-1});
+        // const firstId=_.get(_.first(getPreviouse_Id),'e_Id',999);
+        // const newId=firstId+1;
+        // Object.assign(req.body,{e_Id:newId});
         const addingStudent=new student(req.body);
         
-        if(req.body.wantScholar!='yes' && req.body.discription!=""){
-            res.status(400).send({
-                message: "Discription is not required."
-            });
-        }
+        // if(req.body.wantScholar!='yes' && req.body.discription!=""){
+        //     res.status(400).send({
+        //         message: "Discription is not required."
+        //     });
+        // }
         const insertStudent=await addingStudent.save();
         res.status(201).send(insertStudent);
     }
